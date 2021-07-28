036665d9787697441cd9fe7609714e400ea3efeb
Direct-Dev-Ru <info@direct-dev.ru>
2021-07-28 20:30:26 +0600

    20210728-006

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ const port = process.env.PORT || 8000;
 
 const indexRouter = require('./routes/index');
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
   }
 `;
-const resolvers = { Query: { hello: () => 'Hello world!' } };
+const resolvers = {
+  Query: { hello: () => 'Hello world!', notes: () => notes }
+};
 
 const app = express();
 app.use(logger('dev'));
