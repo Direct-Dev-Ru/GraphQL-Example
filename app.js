2e5d8056d76c3764a82ecf7ed6bbf8cf89ec0cb1
Direct-Dev-Ru <info@direct-dev.ru>
2021-07-28 15:24:35 +0600

    20210728-001

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { ApolloServer, gql } = require('apollo-server-express');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const port = process.env.PORT || 8000;
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -17,6 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-var listener = app.listen(8080, function() {
+const listener = app.listen(port, function() {
   console.log("Listening on port " + listener.address().port);
 });
