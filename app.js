f42c2466c0e2076759059f4615922a445ce93660
Direct-Dev-Ru <info@direct-dev.ru>
2021-07-28 15:46:25 +0600

    20210728-002

--- app.js
+++ app.js
@@ -2,24 +2,38 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer, gql } = require("apollo-server-express");
 
 const port = process.env.PORT || 8000;
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
-const app = express();
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = { Query: { hello: () => "Hello world!" } };
 
+const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+//Настраиваем Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
+server.applyMiddleware({ app, path: "/api" });
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-const listener = app.listen(port, function() {
-  console.log("Listening on port " + listener.address().port);
+const listener = app.listen(port, function () {
+  console.log(
+    `GraphQL Server running at http://localhost:${listener.address().port}${
+      server.graphqlPath
+    }`
+  );
 });
