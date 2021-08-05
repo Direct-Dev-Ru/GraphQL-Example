69292dea5f6ebebc17570b16be9399a5370533c5
Direct-Dev-Ru <info@direct-dev.ru>
2021-08-05 17:36:11 +0600

    20210805-001

--- src/data/graphql/resolvers/mutation.js
+++ src/data/graphql/resolvers/mutation.js
@@ -1,10 +1,22 @@
-const Mutation = {
-  newNote: async (parent, args, { models }) => {
-    return await models.Note.create({
-      content: args.content,
-      author: 'Adam Scott'
-    });
+const newNote = async (parent, args, { models }) => {
+  return await models.Note.create({
+    content: args.content,
+    author: 'Adam Scott'
+  });
+};
+
+const deleteNote = async (parent, { id }, { models }) => {
+  try {
+    await models.Note.findOneAndRemove({ _id: id });
+    return true;
+  } catch (err) {
+    return false;
   }
+};
+
+const Mutation = {
+  newNote,
+  deleteNote
 };
 
 module.exports = Mutation;
