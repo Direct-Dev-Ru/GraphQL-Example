8c2ba5d9c01260871115426ad2ed2270eb9a86c9
Direct-Dev-Ru <info@direct-dev.ru>
2021-08-06 19:34:21 +0600

    20210806-002 Begin auth functional adding

---
+++ src/data/mongodb/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, index: { unique: true } },
+    email: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true },
+    avatar: { type: String }
+  },
+  {
+    // Присваиваем поля createdAt и updatedAt с типом данных
+    timestamps: true
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
